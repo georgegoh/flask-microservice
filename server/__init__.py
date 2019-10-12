04674a9a77f6ed87a670ea4f5388aa561fec3307
George Goh <georgegohkokleong@gmail.com>
2019-10-12 01:51:39 +0000

    Implement stock levels and increment/decrement methods.

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, abort, session, request, redirect
 from flask.json import jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, template_folder="../public", static_folder="../public", static_url_path='')
+app.config["MONGO_URI"] = "mongodb://support-0.ocp3.lab.spodon.com:27017/test"
+mongo = PyMongo(app)
 
 from server.routes import *
 from server.services import *
--- server/routes/index.py
+++ server/routes/index.py
@@ -1,5 +1,8 @@
-from server import app
-from flask import render_template
+from server import app, mongo
+from flask import render_template, jsonify
+from flask_cors import CORS
+
+CORS(app)
 
 @app.route('/')
 def hello_world():
@@ -14,3 +17,29 @@ def page_not_found(error):
 @app.route("/error500")
 def requests_error(error):
     return app.send_static_file('500.html')
+
+@app.route('/api/v1/stocklevels')
+def getstock():
+    products = []
+    for p in mongo.db.products.find({}):
+        p.pop("_id")
+        products.append(p)
+    return jsonify(products)
+
+@app.route('/api/v1/decrement/<int:sku>')
+def decrementstock(sku):
+    print("decrement SKU: %d" % sku)
+    p = mongo.db.products.find_one({"sku": sku})
+    updated_qty = p['qty'] - 1
+    update = {"$set": {"qty": updated_qty}}
+    mongo.db.products.update_one({"sku": sku}, update)
+    return 'OK %d' % updated_qty
+    
+@app.route('/api/v1/increment/<int:sku>')
+def incrementstock(sku):
+    print("increment SKU: %d" % sku)
+    p = mongo.db.products.find_one({"sku": sku})
+    updated_qty = p['qty'] + 1
+    update = {"$set": {"qty": updated_qty}}
+    mongo.db.products.update_one({"sku": sku}, update)
+    return 'OK %d' % updated_qty 
